19e82a5251a9f0326aadde07866b2607aedd8ee1
nghiahoangdayoneteams <nghiahoang@dayoneteams.com>
2021-08-27 21:48:43 +0700

    search folder

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')))
 console.log(__dirname);
 
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // template engines
 app.engine('hbs', handlebars({
@@ -16,14 +16,19 @@ app.engine('hbs', handlebars({
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
     res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+    res.render('search');
+    console.log('ssss', req.query.q)
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-})
+});
