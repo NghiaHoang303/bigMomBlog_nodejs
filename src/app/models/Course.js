df3779e7c85fe2dc800d2d59444c1f132146441b
nghiahoangdayoneteams <nghiahoang@dayoneteams.com>
2021-08-29 15:11:44 +0700

    feature add course

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
 const Schema = mongoose.Schema;
+mongoose.plugin(slug);
 
 const Course = new Schema({
     name: { type: String, maxLength: 255 },
     description: { type: String, maxLength: 800 },
     image: {type: String},
-    createAt: { type: Date, default: Date.now },    
-    updateAt: { type: Date, default: Date.now },    
+    slug: { type: String},
+    level: { type: String},
+    videoId: { type: String},   
+    slug: { type: String, slug: "name", unique: true },
+  }, {
+    timestamps: true,
   });
 
   module.exports = mongoose.model('Course', Course)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 const newRouter = require('./news');
 const siteRouter = require('./site');
 const searchRouter = require('./search');
+const courseRouter = require('./courses');
+
 
 function route(app) {
   app.use('/news', newRouter);
 
   app.use('/search', searchRouter);
+
+  app.use('/courses', courseRouter);
 
   app.use('/', siteRouter);
 }
